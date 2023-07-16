63127e50123840a28c366bcb07ed699756b22d59
KingNelx <tapiajonel606@gmail.com>
2023-07-17 01:43:43 +0800

    implemented task service and impl

--- todo-app-spring/src/main/java/todoappspring/Service/TasksImpl.java
+++ todo-app-spring/src/main/java/todoappspring/Service/TasksImpl.java
@@ -1,5 +1,78 @@
 package todoappspring.Service;
 
-public interface TasksImpl {
-    
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.client.HttpClientErrorException;
+import java.util.List;
+import java.util.Optional;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import todoappspring.Entity.Tasks;
+import todoappspring.Repository.TasksRepo;
+
+@Service
+public class TasksImpl implements TasksService {
+
+    @Autowired
+    private TasksRepo tasksRepo;
+
+    @Override
+    public ResponseEntity<String> createTasks(@RequestBody Tasks createTasks) {
+        Optional<Tasks> doesTaskExist = tasksRepo.findById(createTasks.getId());
+        try {
+            if (doesTaskExist.isPresent()) {
+                throw new HttpClientErrorException(HttpStatus.CONFLICT);
+            }
+            tasksRepo.save(createTasks);
+        } catch (Exception e) {
+            System.out.println(" ERROR " + e);
+        }
+        return ResponseEntity.ok(" NEW TASK ADDED ");
+
+    }
+
+    @Override
+    public List<Tasks> getAllTasks() {
+        if (tasksRepo.findAll().isEmpty()) {
+            throw new HttpClientErrorException(HttpStatus.NO_CONTENT);
+        }
+        return tasksRepo.findAll();
+    }
+
+    @Override
+    public Optional<Tasks> getTaskByID(@PathVariable String id) {
+        if (!tasksRepo.findById(id).isPresent()) {
+            throw new HttpClientErrorException(HttpStatus.NOT_FOUND);
+        }
+        return tasksRepo.findById(id);
+    }
+
+    @Override
+    public ResponseEntity<String> updateTaskByID(@PathVariable String id, @RequestBody Tasks newTasks) {
+        Tasks existingTasks = tasksRepo.findById(id)
+                .orElseThrow(() -> new HttpClientErrorException(HttpStatus.NOT_FOUND));
+
+        existingTasks.setTaskName(newTasks.getTaskName());
+        existingTasks.setTaskDescription(newTasks.getTaskDescription());
+        existingTasks.setDifficulty(newTasks.getDifficulty());
+        existingTasks.setCreatedAt(newTasks.getCreatedAt());
+        existingTasks.setDueDate(newTasks.getDueDate());
+        existingTasks.setStatus(newTasks.getStatus());
+        existingTasks.setPriority(newTasks.getPriority());
+
+        tasksRepo.save(existingTasks);
+
+        return ResponseEntity.ok(" TASKS DATA: " + id + " HAS BEEN UPDATED ");
+    }
+
+    @Override
+    public ResponseEntity<String> deleteTaskByID(@PathVariable String id) {
+        if (!tasksRepo.findById(id).isPresent()) {
+            throw new HttpClientErrorException(HttpStatus.NOT_FOUND);
+        }
+        tasksRepo.deleteById(id);
+        return ResponseEntity.ok(" TASK DATA WITH ID: " + id + " HAS BEEN DELETED ");
+    }
 }
--- todo-app-spring/src/main/java/todoappspring/Service/TasksService.java
+++ todo-app-spring/src/main/java/todoappspring/Service/TasksService.java
@@ -1,5 +1,29 @@
 package todoappspring.Service;
 
-public class TasksService {
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import todoappspring.Entity.Tasks;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+
+public interface TasksService {
     
+    // create tasks
+    ResponseEntity <String> createTasks(Tasks createTask);
+
+    // get tasks
+    List <Tasks> getAllTasks();
+
+    // get tasks by id  
+    Optional <Tasks> getTaskByID(String id);
+
+    // update task by id
+    ResponseEntity <String> updateTaskByID(String id, Tasks newTasks);
+
+    // delete task by id
+    ResponseEntity <String> deleteTaskByID(String id);
 }
