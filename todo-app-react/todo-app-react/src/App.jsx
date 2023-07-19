583a1871894e7cc5c86627e061c5ce2b4693784f
KingNelx <tapiajonel606@gmail.com>
2023-07-20 05:20:34 +0800

    test

--- todo-app-react/todo-app-react/src/App.jsx
+++ todo-app-react/todo-app-react/src/App.jsx
@@ -1,12 +1,17 @@
-import './App.css'
-
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Start from "./Components/Start";
 
 const App = () => {
-  return ( 
+  return (
     <div>
-
+      <Router>
+        <Routes>
+          <Route path="/" element={<Start />} />
+        </Routes>
+      </Router>
     </div>
-   );
-}
- 
+  );
+};
+
 export default App;
